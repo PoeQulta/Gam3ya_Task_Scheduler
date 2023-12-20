eea6e3720df12b8f7f78ed26a905c01fb23a8948
Pola Qulta <poequlta199@gmail.com>
2023-12-20 16:31:21 +0200

    deadlock fixup

--- Gam3ya.cpp
+++ Gam3ya.cpp
@@ -3,11 +3,12 @@
 #include <thread>
 #include <random>
 #include <semaphore.h>
-#define NUM_PROC 20
-#define TimeStep 100
+#include <unistd.h>
+#define NUM_PROC 100
+#define TimeStep 500
 using namespace std;
 sem_t cpuMutex;
-
+int TerminatedCount;
 class process
 {
     public:
@@ -31,56 +32,60 @@ class QueueLvl
     public:
     queue<process> intQueue;
     int Q;
-    QueueLvl nxtLvl;
-    QueueLvl prevLvl;
+    QueueLvl *nxtLvl;
+    QueueLvl *prevLvl;
+    int isFull;
     sem_t full;
     sem_t empty;
-    sem_t queueMutex;
     int sleepTime;
-    QueueLvl(int q, int size, int freq, QueueLvl prev, QueueLvl nxt)
+    QueueLvl(int q, int size, int freq, QueueLvl *prev, QueueLvl *nxt)
     {
         this->Q = q;
         this->nxtLvl = nxt;
         this->prevLvl = prev;
         this->sleepTime = ((float)100/freq) * TimeStep;
+        this->isFull = 0;
         sem_init(&full,0,0);
         sem_init(&empty,0,size);
-        sem_init(&queueMutex,0,1);
     }
     void enqueue(process proc)
     {
+        int val;
         sem_wait(&empty);
-        sem_wait(&queueMutex);
             intQueue.push(proc);
-        sem_post(&queueMutex);
         sem_post(&full);
+        sem_getvalue(&empty,&val);
+        isFull = !val;
     }
     void Run()
     {
         while(1)
         {
             int TaskDone;
-            sem_wait(&full);  
+            sem_wait(&full);
             sem_wait(&cpuMutex);
             process Temp = intQueue.front();
             intQueue.pop();
             TaskDone = Temp.Execute(Q);
             if(nxtLvl == NULL)
+            {
                 TaskDone = 1;
+                cout << "Task in FCFS" << endl;
+            } 
             if(TaskDone)
             {
-                cout << "Process w id: " << Temp.id << " Finished with initial Burst time: " << Temp.initBurstTime << endl; 
+                TerminatedCount++;
+                cout << "Process w id: " << Temp.id << " Finished with initial Burst time: " << Temp.initBurstTime << " Finished: " << TerminatedCount <<  endl; 
             }
             sem_post(&cpuMutex);
-
+            sem_post(&empty);
             if(!TaskDone)
             {
-                if(prevLvl == NULL || (rand() %2))
-                    nxtLvl.enqueue(Temp);
+                if(prevLvl == NULL || (rand()%2) || prevLvl->isFull)
+                    nxtLvl->enqueue(Temp);
                 else
-                    prevLvl.enqueue(Temp);
-            }
-            sem_post(&empty);
+                    prevLvl->enqueue(Temp);
+            }  
             usleep(sleepTime);
         }
     }
@@ -89,6 +94,7 @@ class QueueLvl
 
 int main()
 {
+    TerminatedCount = 0;
     sem_init(&cpuMutex,0,1);
     random_device rd; // obtain a random number from hardware
     mt19937 gen(rd()); // seed the generator Twister pseudoRandom Generator
@@ -96,16 +102,18 @@ int main()
     QueueLvl fcfs = QueueLvl(0,30,20,NULL,NULL);
     QueueLvl lvl2 = QueueLvl(16,20,30,NULL,NULL);
     QueueLvl lvl1 = QueueLvl(8,10,50,NULL,NULL);
-    lvl1.nxtLvl = lvl2;
-    lvl2.prevLvl = lvl1;
-    lvl2.nxtLvl = fcfs;
+    lvl1.nxtLvl = &lvl2;
+    lvl2.prevLvl = &lvl1;
+    lvl2.nxtLvl = &fcfs;
+    fcfs.prevLvl = &lvl2;
     std::thread thread_1(&QueueLvl::Run, &lvl1);
     std::thread thread_2(&QueueLvl::Run, &lvl2);
-    std::thread thread_2(&QueueLvl::Run, &fcfs);
-    process e;
+    std::thread thread_3(&QueueLvl::Run, &fcfs);
     for (int i = 0; i < NUM_PROC; i++)
     {
-        e = process(i,distr(gen));
-        lvl1.enqueue(e);
-    }   
+        lvl1.enqueue(process(i,distr(gen)));
+    }  
+    thread_1.join();
+    thread_2.join();
+    thread_3.join();
 }
